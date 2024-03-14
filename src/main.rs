fab8de6d5c1c6813154374b9a5458f0d5b4c7351
Tomoya Nagasawa <ocrybit@gmail.com>
2024-03-15 01:21:53 +0400

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, Result};
+
+fn command() -> Result<()>{
+    println!("enter command: 1) done");
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer)?;
+    match buffer.as_str().trim_end() {
+	"1" | "done" => {
+	    println!("bye!");
+	}
+	_ => {
+	    println!("command not found...{}", buffer.trim_end());
+	    return command()
+	}
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    command()
 }
