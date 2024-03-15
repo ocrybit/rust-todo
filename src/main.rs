6d983a611bf4231bb026e2b47658b46b596ee49b
Tomoya Nagasawa <ocrybit@gmail.com>
2024-03-15 04:54:33 +0400

    save todos to file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,68 @@
-use std::io::{stdin, Result};
+use std::io::{stdin, Result, BufRead, BufReader};
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+    
+struct Task {
+    name: String,
+}
 
-fn command() -> Result<()>{
-    println!("enter command: 1) done");
+fn save(todos: & Vec<Task>) -> Result<()>{
+    let path = Path::new(".todos/todos.txt");
+    println!("{}",path.display());
+    let mut file = File::create(&path)?;
+    let todos_str = todos.iter()
+	.map(|task| format!("{}", task.name))
+	.collect::<Vec<String>>().join("\n");
+    file.write_all(todos_str.as_bytes())
+}
+
+fn load() -> Result<Vec<Task>>{
+    let path = Path::new(".todos/todos.txt");
+    let file = File::open(&path)?;
+    let buf_reader = BufReader::new(file);
+    let mut tasks = Vec::new();
+    for  line in buf_reader.lines() {
+	let name = line?;
+	tasks.push(Task {
+	    name
+	});
+    }
+    Ok(tasks)
+}
+
+fn add (todos: &mut Vec<Task>) -> Result<()>{
+    println!("enter task({}):", todos.len());
     let mut buffer = String::new();
     stdin().read_line(&mut buffer)?;
-    match buffer.as_str().trim_end() {
-	"1" | "done" => {
+    todos.push(Task { name: buffer.trim().to_string() });
+    let _ = save(todos)?;
+    println!("added {}", todos[todos.len() - 1].name);
+    Ok(())
+}
+
+fn command(todos: &mut Vec<Task>) -> Result<()>{
+    println!("enter command: 1) add, 2) done");
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer)?;
+    match buffer.as_str().trim() {
+	"1" | "add" => {
+	    let res = add(todos);
+	    println!("{:?}", res);
+	    return command(todos)
+	}
+	"2" | "done" => {
 	    println!("bye!");
 	}
 	_ => {
-	    println!("command not found...{}", buffer.trim_end());
-	    return command()
+	    println!("command not found...{}", buffer.trim());
+	    return command(todos)
 	}
     }
     Ok(())
 }
 
 fn main() -> Result<()> {
-    command()
+    let mut todos = load()?;
+    command(&mut todos)
 }
